59413c73e53cf9a647480cc4aafe91bcbf0cfa82
Alina <anikeevaaa99@gmail.com>
2024-02-21 23:12:26 +0300

    task12 refactor

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -38,7 +38,6 @@ class SearchActivity : AppCompatActivity() {
     private lateinit var returnItemImageView: ImageView
 
     private lateinit var inputEditText: EditText
-    private lateinit var placeholderMessage: TextView
 
     private var lastRequest: String = ""
     var inputSaveText: String = ""
@@ -68,6 +67,8 @@ class SearchActivity : AppCompatActivity() {
         addChangeListeners()
         addButtonListeners()
 
+        historySearchGroup.isVisible = searchHistory.get().isNotEmpty()
+
         // Кнопка очищения истории поиска
         cleanHistoryButton.setOnClickListener {
             sharedPrefs.edit().clear().apply()
@@ -78,19 +79,12 @@ class SearchActivity : AppCompatActivity() {
 
         adapter.trackList = trackList
         historyAdapter.trackList = searchHistory.get().toCollection(ArrayList())
+        historyAdapter.notifyDataSetChanged()
 
         inputSaveText = inputEditText.text.toString()
 
         adapter.itemClickListener = { position, track ->
             searchHistory.add(track)
-        }
-    }
-
-    private fun clearButtonVisibility(s: CharSequence?): Int {
-        return if (s.isNullOrEmpty()) {
-            View.GONE
-        } else {
-            View.VISIBLE
         }
     }
 
@@ -117,8 +111,9 @@ class SearchActivity : AppCompatActivity() {
         itunesService.search(lastText)
             .enqueue(object : Callback<ItunesResponse> {
                 @SuppressLint("NotifyDataSetChanged")
-                override fun onResponse(call: Call<ItunesResponse>,
-                                        response: Response<ItunesResponse>
+                override fun onResponse(
+                    call: Call<ItunesResponse>,
+                    response: Response<ItunesResponse>
                 ) {
                     when (response.code()) {
                         200 -> {
@@ -128,12 +123,11 @@ class SearchActivity : AppCompatActivity() {
                                 adapter.notifyDataSetChanged()
                                 recycler.visibility = View.VISIBLE
                                 linearNothingFound.visibility = View.GONE
-                                linearNoInternet.visibility = View.GONE
                             } else {
                                 linearNothingFound.visibility = View.VISIBLE
-                                linearNoInternet.visibility = View.GONE
                                 recycler.visibility = View.GONE
                             }
+                            linearNoInternet.visibility = View.GONE
                         }
                         else -> {
                             linearNoInternet.visibility = View.VISIBLE
@@ -186,18 +180,24 @@ class SearchActivity : AppCompatActivity() {
         // Реагирует на ввод текста в EditText
         inputEditText.requestFocus()
         inputEditText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
             override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                clearButton.isVisible = inputEditText.text.isNotEmpty()
+                recycler.isVisible = inputEditText.text.isNotEmpty()
+                linearNothingFound.isVisible = false
                 if (inputEditText.hasFocus() && inputEditText.text.isEmpty()) {
                     historySearchGroup.isVisible = searchHistory.get().isNotEmpty()
-
+                    linearNoInternet.isVisible = false
+                    linearNothingFound.isVisible = false
+                } else {
+                    historySearchGroup.isVisible = false
+                    recycler.isVisible = true
                 }
-                clearButton.visibility = clearButtonVisibility(s)
                 inputSaveText = s.toString()
             }
-            override fun afterTextChanged(p0: Editable?) {
-            }
+
+            override fun afterTextChanged(p0: Editable?) {}
         })
         // Реагирует на нажатие кнопки на клавиатуре и выполняет поиск
         inputEditText.setOnEditorActionListener { _, actionId, _ ->
@@ -214,10 +214,9 @@ class SearchActivity : AppCompatActivity() {
         linearNothingFound = findViewById(R.id.error_block_nothing_found)
         linearNoInternet = findViewById(R.id.error_block_setting)
         clearButton = findViewById<Button>(R.id.exit)
-        updateButton= findViewById(R.id.button_update)
+        updateButton = findViewById(R.id.button_update)
         returnItemImageView = findViewById<ImageView>(R.id.return_n)
         cleanHistoryButton = findViewById(R.id.clean_history_button)
-        placeholderMessage = findViewById(R.id.placeholderMessage)
         inputEditText = findViewById(R.id.search_content)
         historySearchGroup = findViewById(R.id.history_search_group)
         recycler = findViewById<RecyclerView>(R.id.recyclerView)
