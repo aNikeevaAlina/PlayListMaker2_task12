8b51ee70048999f464769bd34703c0938ebecce8
Alina <anikeevaaa99@gmail.com>
2024-07-09 20:24:33 +0300

    fixes

--- app/src/main/java/com/practicum/playlistmaker/search/data/NetworkServiceImpl.kt
+++ app/src/main/java/com/practicum/playlistmaker/search/data/NetworkServiceImpl.kt
@@ -3,36 +3,19 @@ package com.practicum.playlistmaker.search.data
 import com.practicum.playlistmaker.search.data.model.TrackSearchException
 import com.practicum.playlistmaker.search.domain.NetworkService
 import com.practicum.playlistmaker.search.domain.Track
-import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.callbackFlow
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.flow.flow
 
 class NetworkServiceImpl(private val itunesApi: ApiForItunes) : NetworkService {
 
-    override fun search(text: String): Flow<List<Track>> {
-        return callbackFlow {
-            val callback = object : Callback<ItunesResponse> {
-                override fun onResponse(
-                    call: Call<ItunesResponse>,
-                    response: Response<ItunesResponse>
-                ) {
-                    val body = response.body()
-                    if (response.isSuccessful && body != null) {
-                        trySend(body.results)
-                    } else {
-                        throw TrackSearchException("The data is null")
-                    }
-                }
-
-                override fun onFailure(call: Call<ItunesResponse>, t: Throwable) {
-                    throw TrackSearchException(t.message)
-                }
-            }
-            itunesApi.search(text).enqueue(callback)
-            awaitClose { }
+    override fun search(text: String): Flow<List<Track>> = flow {
+        val response = itunesApi.search(text)
+        val body = response.body()
+        if (response.isSuccessful) {
+            if (body != null) emit(body.results)
+            else throw TrackSearchException("The data is null")
+        } else {
+            throw TrackSearchException()
         }
     }
 }
